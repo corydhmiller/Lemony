af3776c78fceaa3949e777817292ca6b635a34ef
Cory Miller <corydhmiller@gmail.com>
2018-12-20 08:42:11 +0000

    Update animations for css transition

--- scripts/src/scripts.js
+++ scripts/src/scripts.js
@@ -1,4 +1,4 @@
-function vlogologyScripts() {
+(function () {
   const $sectionToggle = $(".sidebar__section--toggle");
   const $sectionHidden = $(".sidebar__section--hidden");
   const $sectionToggledClass = "sidebar__section--toggled";
@@ -35,6 +35,45 @@ function vlogologyScripts() {
     }
   });
 
+   const setHeightToContents = el => {
+     return el.height(el[0].scrollHeight);
+   };
+
+   $('.animate').each(function() {
+    const el = $(this);
+     setHeightToContents(el);
+   });
+
+  //  $('.animate__flyin').each(function() {
+  //    const el = $(this);
+  //    const delayTime = el.data("delay");
+  //    setTimeout(function () {
+  //      el.addClass('animate__flyin--complete');
+  //    }, delayTime ? delayTime : "0");
+  //  });
+
+   $(window).scroll(function () {
+     if (checkVisible($('.animate'))) {
+       $('.animate__flyin').each(function () {
+         const el = $(this);
+         const delayTime = el.data("delay");
+         setTimeout(function () {
+           el.addClass('animate__flyin--complete');
+         }, delayTime ? delayTime : "0");
+       });
+     }
+   });
+
+   function checkVisible(elm, evalCheck = "visible") {
+     let vpH = $(window).height(), // Viewport Height
+       st = $(window).scrollTop(), // Scroll Top
+       y = $(elm).offset().top,
+       elementHeight = $(elm).height();
+
+     if (evalCheck == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+     if (evalCheck == "above") return ((y < (vpH + st)));
+   }
+
   (function (l) {
     let i,
       s = {
@@ -42,7 +81,4 @@ function vlogologyScripts() {
       };
     for (i in s) l.addEventListener(i, s);
   })(document);
-}
-$(document).ready(function () {
-  vlogologyScripts();
-});
+})();
